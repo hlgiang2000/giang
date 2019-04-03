b3a30ae29222c2bfe099a0529d088f890e51e22f
hlgiang2000 <49135832+hlgiang2000@users.noreply.github.com>
2019-04-03 10:04:45 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,62 @@
+package baithi
+        ;
+
+public class Mai
+nExam {
+    public static void main(String[] args){
+//        Hotel 1-10
+        Hotel hotel=new Hotel();
+        hotel.InforHotel();
+
+
+        Hotel hotel1=new Hotel();
+        hotel1.InforHotel();
+
+        Hotel hotel2=new Hotel();
+        hotel2.InforHotel();
+
+
+        Hotel hotel3=new Hotel();
+        hotel3.InforHotel();
+
+
+        Hotel hotel4=new Hotel();
+        hotel4.InforHotel();
+
+
+        Hotel hotel5=new Hotel();
+        hotel5.InforHotel();
+
+
+        Hotel hotel6=new Hotel();
+        hotel6.InforHotel();
+
+
+        Hotel hotel7=new Hotel();
+        hotel7.InforHotel();
+
+
+        Hotel hotel8=new Hotel();
+        hotel8.InforHotel();
+
+
+        Hotel hotel9=new Hotel();
+        hotel9.InforHotel();
+
+        hotel.InforHotel();
+        hotel1.InforHotel();
+        hotel2.InforHotel();
+        hotel3.InforHotel();
+        hotel4.InforHotel();
+        hotel5.InforHotel();
+        hotel6.InforHotel();
+        hotel7.InforHotel();
+        hotel8.InforHotel();
+        hotel9.InforHotel();
+
+        Hotel Search=new Hotel();
+        Search.SearchOwnerName("1");
+
+
+    }
+}
